192ed05687284262eff11524c002012a9f6ba66a
Cees-Willem Hofstede <cees-willem@clinicalgraphics.com>
2022-09-04 09:30:52 +0200

    Initial commit, site scaffold, vite+vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <img src="/beer.svg" class="logo" alt="Beer" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+</style>
