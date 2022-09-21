de23872a906f6af156f693d8a1609ae9413b0660
Cees-Willem Hofstede <cees-willem@clinicalgraphics.com>
2022-09-21 21:46:39 +0200

    Smooth shadow transition, new build

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,8 @@
   height: 6em;
   padding: 1.5em;
   will-change: filter;
+  filter: drop-shadow(0 0 2em transparent);
+  transition: all 400ms;
 }
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
